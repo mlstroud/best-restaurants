3b0938de7a37f2e80060674f30d54089f334c5f6
Matt Stroud <lixingke06@gmail.com>
2020-05-28 18:25:45 -0700

    add restaurantscontroller

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestRestaurants.Models
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(restaurants => restaurants.Cuisine).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Restaurant thisRest = _db.Restaurants.FirstOrDefault(restauraunts => restauraunts.RestaurantId == id);
+      return View(thisRest);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Restaurant thisRest = _db.Restaurants.FirstOrDefault(restauraunts => restauraunts.RestaurantId == id);
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
+      return View(thisRest);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Restaurant restaurant)
+    {
+      _db.Entry(restaurant).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Restaurant thisRest = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      return View(thisRest);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Restaurant thisRest = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      _db.Restaurants.Remove(thisRest);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
